5ab5a123470c3f1f32c61e2ce9728210e6c9f4c1
Mihály Vághy <vaghy.mihaly@gmail.com>
2020-12-21 19:55:38 +0100

    Contraints and basic print finished

--- nonogram.py
+++ nonogram.py
@@ -1,5 +1,7 @@
 from gurobipy import *
 import csv
+import matplotlib
+import matplotlib.pyplot
 
 def read_nonogram(filename):
     with open(filename, "r") as file:
@@ -11,8 +13,12 @@ def read_nonogram(filename):
                 s[key][j][k] = int(s[key][j][k])
     return s
 
-def print_nonogram(s):
-    pass
+def print_nonogram(nonogram, filename):
+    matplotlib.pyplot.subplots()
+    ax = matplotlib.pyplot.gca()
+    ax.imshow(nonogram, interpolation="none", cmap=matplotlib.colors.ListedColormap(['w', 'k']))
+    matplotlib.pyplot.axis("off")
+    matplotlib.pyplot.savefig(filename + ".png")
 
 def compute_sizes(s):
     return {key: [len(clusters) for clusters in s[key]] for key in s}
@@ -57,6 +63,25 @@ def solve_nonogram(filename):
                 model.addConstr(x[i][t][j] <= quicksum(x[i][t+1][jp] for jp in range(j,len(x[i][t+1]))))
 
 
+    for i in range(len(y)):
+        for j in range(len(x)):
+            for t in range(len(y[i])):
+                for jp in range(max(e["r"][i][t],j-s["r"][i][t]+1),min(l["r"][i][t],j)+1):
+                    model.addConstr(y[i][t][jp-e["r"][i][t]] <= z[i][j])
+            for t in range(len(x[j])):
+                for ip in range(max(e["c"][j][t],i-s["c"][j][t]+1),min(l["c"][j][t],i)+1):
+                    model.addConstr(x[j][t][ip-e["c"][j][t]] <= z[i][j])
+
+    for i in range(len(y)):
+        for j in range(len(x)):
+            model.addConstr(z[i][j] <= quicksum(quicksum(y[i][t][jp-e["r"][i][t]] for jp in range(max(e["r"][i][t],j-s["r"][i][t]+1),min(l["r"][i][t],j)+1)) for t in range(len(y[i]))))
+            model.addConstr(z[i][j] <= quicksum(quicksum(x[j][t][ip-e["c"][j][t]] for ip in range(max(e["c"][j][t],i-s["c"][j][t]+1),min(l["c"][j][t],i)+1)) for t in range(len(x[j]))))
+
+    model.Params.OutputFlag = 0
+    model.optimize()
+
+    return [[z[i][j].x for j in range(len(z[i]))] for i in range(len(z))]
+
 if __name__ == "__main__":
     solution = solve_nonogram("small.no")
-    print_nonogram(solution)
+    print_nonogram(solution, "small")
